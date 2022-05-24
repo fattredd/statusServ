8af5be5e70ca917b27eaea636b5dce9d9494624c
JButler <fattredd@gmail.com>
2022-05-24 01:36:10 -0400

    Added basic auth

--- src/server.py
+++ src/server.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request
+from flask import Flask, request, abort
+from werkzeug.exceptions import HTTPException
 from markupsafe import escape
-import os, shutil, yaml
+import os, shutil, yaml, hashlib
 import reporter
 app = Flask(__name__)
 
@@ -10,13 +11,32 @@ app = Flask(__name__)
 def show_status():
     return 'Server Status!\n'
 
-@app.route('/report/<servername>', methods=['GET', 'POST'])
-def report(servername):
-    name = escape(servername)
+@app.route('/report/<username>', methods=['GET', 'POST'])
+def report(username):
+    name = escape(username)
+    if not check_user(name, request):
+        abort(401)
     if request.method == 'POST':
         return report.add(name, request.form['data'])
     else:
         return report.get(name)
+
+@app.errorhandler(HTTPException)
+def internal_error(error):
+    return {"status": error.code}
+
+def check_user(name, req):
+    if not 'password' in req.form:
+        return False
+    if not name in config['users'].keys():
+        return False
+    user = config['users'][name]
+    needed_perms = 'w' if req.method == "POST" else 'r'
+    if needed_perms not in user['perms']:
+        return False
+    salted_pass = req.form['password']+config['salt']
+    hashed_pass = hashlib.md5(salted_pass.encode()).hexdigest()
+    return user['pass'] == hashed_pass
 
 if __name__ == '__main__':
     if not os.path.isfile('config/config.yml'):
